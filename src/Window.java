38a6361cfa41f016a9958a545c63cc4d5131a9b5
avery moates <avery.moates17@ou.edu>
2021-10-06 12:02:37 -0500

    Added some comments to describe what my code is doing.

--- src/Window.java
+++ src/Window.java
@@ -24,20 +24,20 @@ public class Window {
 	
 	//This is the method that actually creates the window.
 	public static void createWindow() {
-		frame = new JFrame(windowName);	//This sets the name.
-		frame.setSize(WIDTH,HEIGHT);			//This sets the size.
-		frame.setVisible(true);					//This makes the window show up on the screen.
+		frame = new JFrame(windowName);							//This sets the name.
+		frame.setSize(WIDTH,HEIGHT);							//This sets the size.
+		frame.setVisible(true);									//This makes the window show up on the screen.
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//This will close the window when you press the x button.
-		frame.setLocationRelativeTo(null);		//This will make the window appear in the middle of the screen.
+		frame.setLocationRelativeTo(null);						//This will make the window appear in the middle of the screen.
 		
-		canvas = new Canvas();
-		canvas.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		canvas = new Canvas();									//This is where other objects will be attached to.
+		canvas.setPreferredSize(new Dimension(WIDTH, HEIGHT));	//Making sure that the size matches the window size.
 		canvas.setMaximumSize(new Dimension(WIDTH, HEIGHT));
 		canvas.setMinimumSize(new Dimension(WIDTH, HEIGHT));
-		canvas.setBackground(Color.RED);
+		canvas.setBackground(Color.RED);						//This is to set the background.
 		
-		frame.add(canvas);
-		frame.pack();
+		frame.add(canvas);										//Attaching the Canvas object to the JFrame object.
+		frame.pack();											//PAcking everything together.
 		
 	}
 
